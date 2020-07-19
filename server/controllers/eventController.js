1c6638ac800b529725a8350031665ae18356665b
Fletcher Jones <fletchrejones@gmail.com>
2020-07-18 23:55:59 -0700

    set up event controller and did create event route

---
+++ server/controllers/eventController.js
@@ -0,0 +1,27 @@
+const Event = require('../models/Event');
+const { validationResult } = require('express-validator');
+
+exports.createEvent = async (req, res, next) => {
+    // check errors
+    const errors = validationResult(req);
+    if( !errors.isEmpty() ) {
+        return res.status(400).json({errors: errors.array() })
+    }
+
+
+    try {
+        // Create a new event
+        const event = new Event(req.body);
+
+        // Save the Creator via JWT
+        event.creator = req.user.user.id;
+
+        // save event
+        event.save();
+        res.json(event);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('There was an error, try it again');
+    }
+}

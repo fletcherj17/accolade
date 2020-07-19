e4993b52d6c9463384bed0c1b052ce7220885a9f
Fletcher Jones <fletchrejones@gmail.com>
2020-07-18 23:51:48 -0700

    added auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env'});
+
+module.exports = (req, res, next ) => {
+    const authHeader = req.get('Authorization');
+
+    if(authHeader) {
+        // get token
+        const token = authHeader.split(' ')[1];
+
+        // console.log(token)
+
+        if(token) {
+            // check JWT
+            try {
+                const user = jwt.verify(token, process.env.SECRET );
+                req.user = user;
+            } catch (error) {
+                console.log(error);
+                console.log('JWT invalid');
+            }
+        }
+
+
+    } 
+
+    return next();
+}

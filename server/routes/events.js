55d2e4499f96bf8a4f73e6af7faa79ca74511025
Fletcher Jones <fletchrejones@gmail.com>
2020-07-19 00:04:26 -0700

    create route for event posts

--- server/routes/events.js
+++ server/routes/events.js
@@ -3,3 +3,16 @@ const router = express.Router();
 const eventController = require('../controllers/eventController');
 const auth = require('../middleware/auth');
 const { check } = require('express-validator');
+
+// Create posts
+// api/posts
+router.post('/', 
+    auth,
+    [
+        check('title', 'The post needs a title').not().isEmpty(),
+        check('type', 'Please select a type').not().isEmpty(),
+        check('details', 'Please enter details').not().isEmpty(),
+        check('city', 'Please select a city').not().isEmpty()
+    ],
+    postController.createPost
+);

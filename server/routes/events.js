dfe0daec09fac142271b2d470187cc209b03d23b
Fletcher Jones <fletchrejones@gmail.com>
2020-07-19 00:08:44 -0700

    create event route

--- server/routes/events.js
+++ server/routes/events.js
@@ -4,3 +4,16 @@ const eventController = require('../controllers/eventController');
 const auth = require('../middleware/auth');
 const { check } = require('express-validator');
 
+// Create events
+// api/events
+router.post('/', 
+    auth,
+    [
+        check('title', 'The event needs a title').not().isEmpty(),
+        check('type', 'Please select a type').not().isEmpty(),
+        check('details', 'Please enter details').not().isEmpty(),
+        check('city', 'Please select a city').not().isEmpty(),
+        check('date', 'When is this event?').not().isEmpty()
+    ],
+    eventController.createEvent
+);

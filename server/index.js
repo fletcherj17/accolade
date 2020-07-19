808c0e6149fa6dbc6619b5ce56b9407d1d652114
Fletcher Jones <fletchrejones@gmail.com>
2020-07-18 23:48:58 -0700

    added routes to server file

--- server/index.js
+++ server/index.js
@@ -17,6 +17,10 @@ app.use( express.json({ extended: true }));
 // App port
 const port = process.env.PORT || 4000;
 
+// Import routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+
 // Start app
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server working at port ${port}`);

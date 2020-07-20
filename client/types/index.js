2c79fb01aae44cb2ce3c6246accffd38add1125c
Fletcher Jones <fletchrejones@gmail.com>
2020-07-20 13:32:54 -0400

    types folder created

---
+++ client/types/index.js
@@ -0,0 +1,30 @@
+export const SIGNUP_OK = 'SIGNUP_OK';
+export const SIGNUP_ERROR = 'SIGNUP_ERROR';
+export const HIDE_ALERT = 'HIDE_ALERT';
+export const LOGIN_OK = 'LOGIN_OK';
+export const LOGIN_ERROR = 'LOGIN_ERROR';
+export const USER_AUTENTICATED = 'USER_AUTENTICATED';
+export const LOG_OUT = 'LOG_OUT';
+
+export const EVENT_FORM = 'EVENT_FORM';
+export const EVENT_UPDATE_FORM = 'EVENT_UPDATE_FORM';
+export const GET_EVENTS = 'GET_EVENTS';
+export const ADD_EVENT = 'ADD_EVENT';
+export const UPDATE_EVENT = 'UPDATE_EVENT';
+export const EVENT_ERROR = 'EVENT_ERROR';
+export const VALIDATE_EVENT = 'VALIDATE_EVENT';
+export const CURRENT_EVENT = 'CURRENT_EVENT';
+export const DELETE_EVENT = 'DELETE_EVENT';
+
+export const POST_FORM = 'POST_FORM';
+export const POST_UPDATE_FORM = 'POST_UPDATE_FORM';
+export const UPDATE_POST = 'UPDATE_POST';
+export const GET_POSTS = 'GET_POSTS';
+export const ADD_POST = 'ADD_POST';
+export const POST_ERROR = 'POST_ERROR';
+export const VALIDATE_POST = 'VALIDATE_POST';
+export const CURRENT_POST = 'CURRENT_POST';
+export const DELETE_POST = 'DELETE_POST';
+export const GET_CREATOR = 'GET_CREATOR';
+
+export const SPINNER_ON = 'SPINNER_ON';

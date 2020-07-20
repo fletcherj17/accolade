dfb4e163b252de80f9e97fb17cf9d7e5c85fc623
Fletcher Jones <fletchrejones@gmail.com>
2020-07-20 13:33:28 -0400

    next configuration added

---
+++ client/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+        //backendURL: 'https://warm-shore-31913.herokuapp.com/',
+        backendURL: 'http://localhost:4000',
+        frontendURL: 'http://localhost:3000',
+        API_KEY: 'AIzaSyDFfsSlkxl1DI0ztyZDRhOPKmIUhK1SuCw'
+    }
+}
